c9b8b247ab04be0a27751bb749c01e002d0dad17
FedoraStegi <sirstegi@gmail.com>
2023-09-08 19:03:06 +0200

    Fixed Pomodoro Timer Issue when pressing Start Button multiple times

--- pomodoro_timer/index.js
+++ pomodoro_timer/index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const startEl = document.getElementById("start");
 const stopEl = document.getElementById("stop");
 const resetEl = document.getElementById("reset");
@@ -7,32 +9,37 @@ let interval;
 let timeLeft = 1500;
 
 function updateTimer() {
-    let minutes = Math.floor(timeLeft / 60);
-    let seconds = timeLeft % 60;
-    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+  let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
 
-    timerEl.innerHTML = formattedTime;
+  timerEl.innerHTML = formattedTime;
 }
 
 function startTimer() {
-interval = setInterval(()=>{
-  timeLeft--;
-  updateTimer();
-  if (timeLeft === -1) {
-    clearInterval(interval);
-    alert("Time's up!");
-    timeLeft = 1500;
+  interval = setInterval(() => {
+    timeLeft--;
     updateTimer();
-  }
+    if (timeLeft === -1) {
+      clearInterval(interval);
+      alert("Time's up!");
+      timeLeft = 1500;
+      updateTimer();
+    }
   }, 1000);
+  startEl.disabled = true;
 }
 function stopTimer() {
-    clearInterval(interval);
+  clearInterval(interval);
+  startEl.disabled = false;
 }
 function resetTimer() {
-    clearInterval(interval);
-    timeLeft = 1500;
-    updateTimer();
+  clearInterval(interval);
+  timeLeft = 1500;
+  updateTimer();
+  startEl.disabled = false;
 }
 
 startEl.addEventListener("click", startTimer);
